5e89285f665a16a7551c4c44918ffd71fd68764a
TMTSUGAR <tmtsugar@mail.ru>
2023-10-16 20:55:15 +0300

    факт о 422

---
+++ 422 is 420 too.py
@@ -0,0 +1,13 @@
+def PENIS (x, y):
+    """Suck My Ass"""
+    Vagina = x - 418
+    result = y - Vagina
+    return result
+
+
+def JOINT (x, y):
+    """Print This Shit"""
+    print(str(y) + ' is ' + str(PENIS (x, y)) + ' too ^_^ ')
+
+
+JOINT(x=420,y=422)
